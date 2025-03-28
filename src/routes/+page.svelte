4030d3adc034e1113c028ac35f247fcb6fcf6003
Michael Sprauer <Michael.Sprauer@sap.com>
2025-03-28 16:22:30 +0100

    feat: 🌍 Integrate Leaflet for map functionality and update configurations
    Add Leaflet integration to display a map on the main page.
    Updated the SvelteKit adapter to static and modified the test imports
    to reflect the new structure. Updated dependencies for better
    compatibility and performance.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Map from 'leaflet';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		let map = Map.map('map').setView([51.505, -0.09], 13); // Default coordinates and zoom level
+		Map.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+		}).addTo(map);
+	});
+</script>
+
+<div id="map"></div>
+
+<style>
+	#map {
+		height: 100vh;
+		width: 100%;
+	}
+</style>
