58d4c3fd781307e4473cb406ccc7a8d4d167ed8a
Michael Sprauer <Michael.Sprauer@sap.com>
2025-04-17 21:32:00 +0200

    chore: add CORS headers to Svelte configuration 🌐
    Include 'Access-Control-Allow-Origin' header to allow requests from
    https://schnitzel.sprauer.net. This change enhances the application's
    cross-origin resource sharing capabilities.

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,12 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter(),
+		headers: {
+			'Access-Control-Allow-Origin': 'https://schnitzel.sprauer.net'
+		}
+	}
 };
 
 export default config;
